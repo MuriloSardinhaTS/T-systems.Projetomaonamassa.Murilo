4fa34b0df1c9cae1b963d6c6b17c1fd72bc573a2
MuriloSardinhaTS <Murilo.Sardinha@t-systems.com>
2026-02-26 14:43:06 -0300

    Add project files.

---
+++ T-systems.Projetomaonamassa.m/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Verificacao
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite sua idade para a verificacao: ");
+        int idade = int.Parse(Console.ReadLine());
+        if (idade < 18)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Você não tem permissão de idade!");
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("OK, você tem a permissão de idade");
+        }
+        Console.ResetColor();
+        Console.ReadKey();
+
+
+    }
+}
